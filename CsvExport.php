f3b980545c53808cba92f8ad2961a86fe815e171
n.vanderburg <nvanderburg@proximity.es>
2015-06-24 12:10:25 +0200

    Setup

---
+++ CsvExport.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace proximity\csvexport;
+
+/**
+ * This is just an example.
+ */
+class CsvExport extends \yii\base\Widget
+{
+    /**
+     * @var array Data rows
+     */
+    public $data = [];
+    /**
+     * @var bool Download csv
+     */
+    public $download = true;
+    /**
+     * @var string the directory where to save the csv file
+     */
+    public $saveDir = "";
+    /**
+     * @var string the resulting filename
+     */
+    public $filename = "export.csv";
+
+    /**
+     * Init widget
+     */
+    public function init(){
+        parent::init();
+        if(!$this->saveDir) $this->saveDir = \Yii::$app->basePath."/runtime";
+        if(!file_exists($this->saveDir)) mkdir($this->saveDir, 0777, true);
+
+    }
+
+    /**
+     * run Widget
+     */
+    public function run(){
+        $filename = $this->filename;
+        $res = $this->data;
+        $campos = false;
+
+        if($this->download){
+            $fp = fopen('php://output', 'w');
+            header("Content-type: application/vnd.ms-excel;charset=utf-8");
+            header( 'Content-Disposition: attachment;filename='.$filename);
+        }else{
+            $fp = fopen($this->saveDir."/".$this->filename, 'w');
+        }
+        foreach($res as $data){
+            if(!$campos){
+                $campos = array_keys($data);
+                fputcsv($fp, $campos, ";");
+            }
+            $fila = array_map('utf8_decode', $data);
+            fputcsv($fp, $fila, ";");
+        }
+        fclose($fp);
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+Yii2 CSV Export
+===============
+simple csv export
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist proximity/yii2-csv-export "*"
+```
+
+or add
+
+```
+"proximity/yii2-csv-export": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \proximity\csvexport\AutoloadExample::widget(); ?>```
